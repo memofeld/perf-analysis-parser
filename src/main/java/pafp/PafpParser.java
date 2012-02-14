5a374f57878694a1384d7b399c13cb8e32426382
akleiner <andreas.kleiner@prodyna.de>
2012-02-14 11:06:06 +0100

    Parser now creates csv output.

--- src/main/java/pafp/PafpParser.java
+++ src/main/java/pafp/PafpParser.java
@@ -10,6 +10,9 @@ public class PafpParser {
 	private String machine;
 	private String iteration;
 	private String user;
+	private String avgPing;
+	private String avgDownload;
+	private String timeSpent;
 	
 	public PafpParser(String filename) {
 		fli = new FileLineIterable(filename);
@@ -18,11 +21,11 @@ public class PafpParser {
 	
 	public PafpParser(File file) {
 		fli = new FileLineIterable(file);
+		setUser(file.getName());
 	}
 	
 	public void setMachine(String line) {
 		machine = line.replaceAll("^.+:\\s", "").replaceAll(",.*", "");
-		System.out.println(machine);
 	}
 	
 	public String getMachine() {
@@ -31,7 +34,6 @@ public class PafpParser {
 	
 	public void setIteration(String line) {
 		iteration = line.replaceAll("@iteration:", "");
-		System.out.println(iteration);
 	}
 	
 	public String getIteration() {
@@ -40,11 +42,34 @@ public class PafpParser {
 	
 	public void setUser(String filename) {
 		user = filename.replaceAll("^.+[\\d- ]+ ", "").replaceAll(".txt", "");
-		System.out.println(user);
 	}
 	
 	public String getUser() {
 		return user;
+	}
+	
+	public void setAvgPing(String line) {
+		avgPing = line.replaceAll("^.+Mittelwert = ", "");
+	}
+	
+	public String getAvgPing() {
+		return avgPing;
+	}
+	
+	public void setAvgDownload(String line) {
+		avgDownload = line.replaceAll("^100 11.0M  100 11.0M    0     0\\s+", "").split(" ")[0];
+	}
+	
+	public String getAvgDownload() {
+		return avgDownload;
+	}
+	
+	public void setTimeSpent(String line) {
+		timeSpent = line.replaceAll("^100 11.0M  100 11.0M    0     0\\s+", "").replaceFirst("[\\dkM]+", "").trim().replaceFirst("0", "").trim().split(" ")[0];
+	}
+	
+	public String getTimeSpent() {
+		return timeSpent;
 	}
 	
 	/**
@@ -56,19 +81,42 @@ public class PafpParser {
 		StringBuffer sb = new StringBuffer();
 		String csv = null;
 		
-		sb.append("Iteration;Machine;User;www.google.com_avg-time;www.heise.de_avg-time;www.die-pa.de_avg-time;intranet.die-pa.de_avg-time;www.prodyna.de_avg-time;support.prodyna.com_avg-time;speedtest_avg-download;speedtest_time-spent;www.die-pa.de_avg-download;www.die-pa.de_time-spent;hetzner_avg-download;hetzner_time-spent\n");
+		sb.append("Iteration;Machine;User;www.google.com_avg-time;www.heise.de_avg-time;www.die-pa.de_avg-time;intranet.die-pa.de_avg-time;www.prodyna.de_avg-time;support.prodyna.com_avg-time;speedtest_avg-download;speedtest_time-spent;www.die-pa.de_avg-download;www.die-pa.de_time-spent;hetzner_avg-download;hetzner_time-spent;\n");
 		
-		int lineNumber = 0;
-		int avgPing = 0;
+		int lineNumber = 1;
+		int lastTimeSpentLineNumber = 0;
+		int timeSpentCounter = 0;
 		for (String s : fli) {
 			
 			
-			if (2 == lineNumber) {
+			if (3 == lineNumber) {
 				setMachine(s);
 			}
 			
 			if (s.matches("^@iteration.+")) {
 				setIteration(s);
+				sb.append(getIteration()).append(";").append(getMachine()).append(getUser()).append(";");
+			}
+			
+			if (s.matches(".*Mittelwert = .+")) {
+				setAvgPing(s);
+				sb.append(getAvgPing()).append(";");
+			}
+			
+			if (s.matches("100 11.0M  100 11.0M.+")) {
+				setAvgDownload(s);
+				setTimeSpent(s);
+				
+				if ((lineNumber - lastTimeSpentLineNumber) > 10) {
+					sb.append(getAvgDownload()).append(";");
+					sb.append(getTimeSpent()).append(";");
+					
+					if (++timeSpentCounter % 3 == 0) {
+						sb.append("\n");
+					}
+					
+					lastTimeSpentLineNumber = lineNumber;
+				}
 			}
 			
 			lineNumber++;
--- src/test/java/pafp/PafpParserTest.java
+++ src/test/java/pafp/PafpParserTest.java
@@ -25,4 +25,44 @@ public class PafpParserTest {
 		
 		assertTrue(machine.equals("23"));
 	}
+
+	@Test
+	public void testUser() throws Exception {
+		PafpParser pafpParser = new PafpParser("data1/perf-20120210-0804 Donald Duck.txt");
+		pafpParser.parse();
+		
+		String machine = pafpParser.getUser();
+		
+		assertTrue(machine.equals("Donald Duck"));
+	}
+
+	@Test
+	public void testAvgPing() throws Exception {
+		PafpParser pafpParser = new PafpParser("data1/perf-20120210-0804 Donald Duck.txt");
+		pafpParser.parse();
+		
+		String machine = pafpParser.getAvgPing();
+		
+		assertTrue(machine.equals("143ms"));
+	}
+
+	@Test
+	public void testAvgDownload() throws Exception {
+		PafpParser pafpParser = new PafpParser("data1/perf-20120210-0804 Donald Duck.txt");
+		pafpParser.parse();
+		
+		String machine = pafpParser.getAvgDownload();
+		
+		assertTrue(machine.equals("149k"));
+	}
+
+	@Test
+	public void testTimeSpent() throws Exception {
+		PafpParser pafpParser = new PafpParser("data1/perf-20120210-0804 Donald Duck.txt");
+		pafpParser.parse();
+		
+		String machine = pafpParser.getTimeSpent();
+		
+		assertTrue(machine.equals("0:01:15"));
+	}
 }
